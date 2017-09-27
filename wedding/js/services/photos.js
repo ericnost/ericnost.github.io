5c4857c01f15dfb79313d0695f67b1dadb3d644b
ericnost <ericnost@gmail.com>
2017-09-27 14:52:41 -0500

    STR
    basic framework

--- wedding/js/services/photos.js
+++
@@ -1,9 +0,0 @@
-app.factory('photos', ['$http', function($http) {
-  return $http.get('data/photosTest.json')
-         .success(function(data) {
-           return data;
-         })
-         .error(function(data) {
-           return data;
-         });
-}]);
